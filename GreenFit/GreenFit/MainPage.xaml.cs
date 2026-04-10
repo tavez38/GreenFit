7d48efd35934bcd645004359a4a47c6f6052711d
tavez38 <tavecchia.lorenzo.22@itisriva.edu.it>
2026-04-10 14:08:53 +0200

    inizio della creazione della UI della home page

--- GreenFit/GreenFit/MainPage.xaml.cs
+++ GreenFit/GreenFit/MainPage.xaml.cs
@@ -8,48 +8,14 @@
         {
             InitializeComponent();
         }
-
-        string? translatedNumber;
-
-        private void OnTranslate(object sender, EventArgs e)
+        private void OnGoogleLoginClicked(object sender, EventArgs e)
         {
-            string enteredNumber = PhoneNumberText.Text;
-            translatedNumber = PhonewordTranslator.ToNumber(enteredNumber);
-
-            if (!string.IsNullOrEmpty(translatedNumber))
-            {
-                CallButton.IsEnabled = true;
-                CallButton.Text = "Call " + translatedNumber;
-            }
-            else
-            {
-                CallButton.IsEnabled = false;
-                CallButton.Text = "Call";
-            }
+        //TODO
+            // Implement Google login logic here
+            // For example, you can use Xamarin.Auth or any other authentication library
+            // to handle Google login and retrieve user information.
         }
 
-        async void OnCall(object sender, System.EventArgs e)
-        {
-            if (await this.DisplayAlertAsync(
-        "Dial a Number",
-        "Would you like to call " + translatedNumber + "?",
-        "Yes",
-        "No"))
-            {
-                // TODO: dial the phone
-            }
-        }
 
-        /* private void OnCounterClicked(object? sender, EventArgs e)
-         {
-             count++;
-
-             if (count == 1)
-                 CounterBtn.Text = $"Clicked {count} time";
-             else
-                 CounterBtn.Text = $"Clicked {count} times";
-
-             SemanticScreenReader.Announce(CounterBtn.Text);
-         }*/
     }
 }
--- GreenFit/GreenFit/obj/Debug/net10.0-windows10.0.19041.0/win-x64/GreenFit.AssemblyInfo.cs
+++ GreenFit/GreenFit/obj/Debug/net10.0-windows10.0.19041.0/win-x64/GreenFit.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GreenFit")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+4e2ccaf29ff6906d0422b4bf3a2fe2d7aab62208")]
 [assembly: System.Reflection.AssemblyProductAttribute("GreenFit")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GreenFit")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
