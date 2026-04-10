4e2ccaf29ff6906d0422b4bf3a2fe2d7aab62208
tavez38 <tavecchia.lorenzo.22@itisriva.edu.it>
2026-04-10 13:22:52 +0200

    creazione del progetto e primi test

---
+++ GreenFit/GreenFit/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace GreenFit
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ GreenFit/GreenFit/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿namespace GreenFit
+{
+    public partial class MainPage : ContentPage
+    {
+       // int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        string? translatedNumber;
+
+        private void OnTranslate(object sender, EventArgs e)
+        {
+            string enteredNumber = PhoneNumberText.Text;
+            translatedNumber = PhonewordTranslator.ToNumber(enteredNumber);
+
+            if (!string.IsNullOrEmpty(translatedNumber))
+            {
+                CallButton.IsEnabled = true;
+                CallButton.Text = "Call " + translatedNumber;
+            }
+            else
+            {
+                CallButton.IsEnabled = false;
+                CallButton.Text = "Call";
+            }
+        }
+
+        async void OnCall(object sender, System.EventArgs e)
+        {
+            if (await this.DisplayAlertAsync(
+        "Dial a Number",
+        "Would you like to call " + translatedNumber + "?",
+        "Yes",
+        "No"))
+            {
+                // TODO: dial the phone
+            }
+        }
+
+        /* private void OnCounterClicked(object? sender, EventArgs e)
+         {
+             count++;
+
+             if (count == 1)
+                 CounterBtn.Text = $"Clicked {count} time";
+             else
+                 CounterBtn.Text = $"Clicked {count} times";
+
+             SemanticScreenReader.Announce(CounterBtn.Text);
+         }*/
+    }
+}
